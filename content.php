182f4bb782c2453d1dd837358a490def506062a4
Joynal Abedin <joynal.9933@gmail.com>
2022-06-13 18:45:18 +0600

    num cal

--- content.php
+++ content.php
@@ -1,3 +1,26 @@
+
+
+<form action="/" method="post">
+    <table>
+        <tr>
+            <th>First Num</th>
+            <td><input type="text" name="f_num"></td>
+        </tr>
+        <tr>
+            <th>Second Num</th>
+            <td><input type="text" name="s_num"></td>
+        </tr>
+        <tr>
+            <td><button name="cal" >Submit</button></td>
+        </tr>
+    </table>
+</form>
+
 <?php 
+if(isset($_POST['cal'])){
+    echo 'yes';
+}else{
+    echo 'no';
+}
 
 
